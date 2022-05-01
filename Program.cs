4e030803e0cb33e9594e6d92bd340a62c0b84b54
n85 <39192863+n85@users.noreply.github.com>
2022-05-01 12:34:54 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QLDiemSV.Entities;
+using QLDiemSV.GUI;
+using System.Configuration;
+
+namespace QLDiemSV
+{
+    
+    internal static class Program
+    {
+        public static string strcon;
+        public static Users user;
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            strcon = ConfigurationManager.ConnectionStrings["strcon"].ConnectionString;
+            Application.Run(new frmLogin());
+        }
+    }
+}
